cb6f3d40f84a423714edaaba44fa3a9d5690b156
Chris <chris.r.lee43@gmail.com>
2023-01-27 21:52:13 -0800

    cancellation modal

--- niftyapes/components/CancelListingModal.tsx
+++ niftyapes/components/CancelListingModal.tsx
@@ -1,19 +1,30 @@
 import {
   Button,
   Heading,
+  HStack,
+  Icon,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  Spinner,
   Text,
   useDisclosure,
   VStack,
 } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import { IoCheckmarkCircle } from 'react-icons/io5'
+import LoadingDots from './LoadingDots'
 
 export default function CancelListingModal() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose: onModalClose } = useDisclosure()
+  const [cancellationSuccess, setCancellationSuccess] = useState(false)
+  const onClose = () => {
+    setCancellationSuccess(false)
+    onModalClose()
+  }
 
   return (
     <>
@@ -35,25 +46,66 @@ export default function CancelListingModal() {
           </ModalHeader>
           <ModalCloseButton />
           <ModalBody p="12">
-            <VStack spacing="2">
-              <Heading size="md">Submit cancellation</Heading>
-              <Text>
-                To cancel this listing you must confirm the transaction and pay
-                the gas fee.
-              </Text>
-            </VStack>
-            <Button
-              w="full"
-              variant="outline"
-              colorScheme="red"
-              _hover={{ bg: 'blackAlpha.100' }}
-              onClick={onClose}
-            >
-              Abort cancellation
-            </Button>
+            {!cancellationSuccess ? (
+              <CancellationInProgress
+                onApprove={() => {
+                  setCancellationSuccess(true)
+                }}
+                onClose={onClose}
+              />
+            ) : (
+              <CancellationSuccess onClose={onClose} />
+            )}
           </ModalBody>
         </ModalContent>
       </Modal>
     </>
+  )
+}
+
+function CancellationInProgress({
+  onClose,
+  onApprove,
+}: {
+  onClose: () => void
+  onApprove: () => void
+}) {
+  // TODO: Replace with actual data.
+  setTimeout(onApprove, 3000)
+
+  return (
+    <VStack spacing="6">
+      <VStack align="left" spacing="2">
+        <Heading size="md">Submit cancellation</Heading>
+        <Text>
+          To cancel this listing you must confirm the transaction and pay the
+          gas fee.
+        </Text>
+      </VStack>
+      <LoadingDots />
+      <Button
+        w="full"
+        variant="outline"
+        colorScheme="red"
+        _hover={{ bg: 'blackAlpha.100' }}
+        onClick={onClose}
+      >
+        Abort cancellation
+      </Button>
+    </VStack>
+  )
+}
+
+function CancellationSuccess({ onClose }: { onClose: () => void }) {
+  return (
+    <VStack spacing="6">
+      <VStack spacing="2">
+        <Heading size="md">Cancellation Success</Heading>
+        <Icon color="green.400" boxSize="20" as={IoCheckmarkCircle} />
+      </VStack>
+      <Button onClick={onClose} colorScheme="blue" w="full">
+        Close
+      </Button>
+    </VStack>
   )
 }
