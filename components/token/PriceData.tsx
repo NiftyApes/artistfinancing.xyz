ad83d204bdf156cf169834af570633adc4092443
Chris <chris.r.lee43@gmail.com>
2023-02-03 15:50:44 -0800

    Add item view ui link to modal

--- components/token/PriceData.tsx
+++ components/token/PriceData.tsx
@@ -174,12 +174,19 @@ const PriceData: FC<Props> = ({ details, collection, isOwner }) => {
       ? true
       : false
 
+  // TODO: Remove mock data
+  const hasFinanceListing = true
+
   return (
     <>
-      {isOwner && (
+      {hasFinanceListing && (
         <div className="col-span-full md:col-span-4 lg:col-span-5 lg:col-start-2">
           <article className="col-span-full rounded-2xl border border-gray-300 bg-white p-6 dark:border-neutral-600 dark:bg-black">
-            <ListFinancingSection token={token} collection={collection} />
+            <ListFinancingSection
+              token={token}
+              collection={collection}
+              isOwner={isOwner}
+            />
           </article>
         </div>
       )}
