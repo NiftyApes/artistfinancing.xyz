2540ebaab47579e0bbff72e95f001101c739d32c
Antonio <hedhogs@gmail.com>
2023-02-14 15:31:08 +0100

    feat(portfolio): add dummy buttons to tabs; #22

--- pages/address/[address].tsx
+++ pages/address/[address].tsx
@@ -160,17 +160,41 @@ const Address: NextPage<Props> = ({ address, fallback }) => {
             </Tabs.Content>
             {isOwner && (
               <Tabs.Content value="upcoming_payments" className="col-span-full">
-                TBD
+                <div className="reservoir-body mt-14 grid justify-center dark:text-white">
+                  <div className="text-center mb-6">
+                    Table
+                  </div>
+                  <div className="flex flex-row justify-center space-x-4">
+                    <button
+                      className="btn-primary-fill gap-2 dark:ring-primary-900 dark:focus:ring-4"
+                    >
+                      Make Payment
+                    </button>
+                    <button
+                      className="btn-primary-outline gap-2 dark:ring-primary-900 dark:focus:ring-4"
+                    >
+                      Sell Loan
+                    </button>
+                  </div>
+                </div>
               </Tabs.Content>
             )}
             {isOwner && (
               <Tabs.Content value="financing_offers" className="col-span-full">
-                TBD
+                <div className="reservoir-body mt-14 grid justify-center dark:text-white">
+                  <button
+                    className="btn-primary-fill gap-2 dark:ring-primary-900 dark:focus:ring-4"
+                  >
+                    Cancel Listing
+                  </button>
+                </div>
               </Tabs.Content>
             )}
             {isOwner && (
               <Tabs.Content value="active_loans" className="col-span-full">
-                TBD
+                <div className="reservoir-body mt-14 grid justify-center dark:text-white">
+                  Loan Terms TBD
+                </div>
               </Tabs.Content>
             )}
             {isOwner && (
