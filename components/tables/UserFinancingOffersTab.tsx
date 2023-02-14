4ebbeb6a2e5dffc88912fdd9d4e7c23e9ad4b990
Antonio <hedhogs@gmail.com>
2023-02-14 15:37:36 +0100

    feat(portfolio): add dummy text to financing offers tab; #22

--- components/tables/UserFinancingOffersTab.tsx
+++ components/tables/UserFinancingOffersTab.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 const UserFinancingOffersTab: React.FC = () => {
   return (
     <div className="reservoir-body mt-14 grid justify-center dark:text-white">
+      <div className="mb-6 text-center">
+        Loan Terms TBD
+      </div>
       <button
         className="btn-primary-fill gap-2 dark:ring-primary-900 dark:focus:ring-4"
       >
