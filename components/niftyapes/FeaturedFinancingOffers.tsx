4064fbd753fbe58a1c49c34600c9b557272ec1eb
Chris <chris.r.lee43@gmail.com>
2023-02-27 16:11:39 -0800

    Featured offers

--- components/niftyapes/FeaturedFinancingOffers.tsx
+++ components/niftyapes/FeaturedFinancingOffers.tsx
@@ -1,19 +1,47 @@
-import { Text } from '@chakra-ui/react'
+import { Grid } from '@chakra-ui/react'
 import { useTokens } from '@reservoir0x/reservoir-kit-ui'
-import useOffers from 'hooks/niftyapes/useOffers'
+import TokenCard from 'components/TokenCard'
+import { getAddress } from 'ethers/lib/utils.js'
+import useOffers, { Offer } from 'hooks/niftyapes/useOffers'
+import { useAccount } from 'wagmi'
 
 export default function FeaturedFinancingOffers() {
+  const { address } = useAccount()
   const { data: offersData, error, isLoading: isLoadingOffers } = useOffers({})
-  const activeOffers = offersData?.filter((offer) => offer.status === 'ACTIVE')
-  const tokensQueryArr = activeOffers?.map(
+  const activeOffers = offersData?.filter(
+    // Show active offers that aren't created by current account
+    (offer) => offer.status === 'ACTIVE' && offer.offer.creator !== address
+  )
+  const tokenQueries = activeOffers?.map(
     (offer) => `${offer.offer.nftContractAddress}:${offer.offer.nftId}`
   )
-  const tokens = useTokens({
-    tokens: tokensQueryArr,
+  const { data: tokens, mutate: tokensMutate } = useTokens({
+    tokens: tokenQueries,
+  })
+  const offersWithTokens = activeOffers?.map((offer) => {
+    const token = tokens.find(
+      (token) =>
+        getAddress(token?.token?.contract || '') ===
+          offer.offer.nftContractAddress &&
+        token?.token?.tokenId === offer.offer.nftId
+    )
+
+    return {
+      offer,
+      token,
+    }
   })
 
-  console.log(tokens)
-  console.log(activeOffers)
-
-  return <Text>womp</Text>
+  return (
+    <Grid templateColumns={'repeat(5, 1fr)'} gap={12}>
+      {offersWithTokens?.map(({ offer, token }) => (
+        <TokenCard
+          token={token}
+          financeOffer={offer}
+          collectionImage={undefined}
+          mutate={tokensMutate}
+        ></TokenCard>
+      ))}
+    </Grid>
+  )
 }
