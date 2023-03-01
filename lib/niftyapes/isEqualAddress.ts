f6b241d57855e4163c6cc088209574f22cdcba50
Chris <chris.r.lee43@gmail.com>
2023-03-01 11:02:54 -0800

    return false for invalid addresses

--- lib/niftyapes/isEqualAddress.ts
+++ lib/niftyapes/isEqualAddress.ts
@@ -7,13 +7,10 @@ export default function isEqualAddress(
   addr1: MaybeAddress,
   addr2: MaybeAddress
 ) {
-  addr1 = addr1 || ''
-  addr2 = addr2 || ''
-
   try {
-    return getAddress(addr1) === getAddress(addr2)
+    return getAddress(addr1 || '') === getAddress(addr2 || '')
   } catch (err) {
     console.warn('Invalid address for compare', err)
-    return addr1.toLowerCase() === addr2.toLowerCase()
+    return false
   }
 }
