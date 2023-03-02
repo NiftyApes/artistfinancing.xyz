737dbd40522b0ab4c019a58f99a4bdb9a62c2d67
andreitr <andrei.taraschuk@gmail.com>
2023-03-02 10:19:07 -0700

    mainnet contract

--- hooks/niftyapes/useNiftyApesContract.ts
+++ hooks/niftyapes/useNiftyApesContract.ts
@@ -1,6 +1,6 @@
 import { Address, goerli, mainnet, useProvider } from 'wagmi'
 
-import { GoerliAbi } from '../../contracts/niftyapes/goerli.abi'
+import { ContractAbi } from '../../contracts/niftyapes/contract.abi'
 import useEnvChain from '../useEnvChain'
 
 const isGoerli = (cid?: number): boolean => {
@@ -18,20 +18,20 @@ export const useNiftyApesContract = () => {
   if (provider && isGoerli(chain?.id)) {
     return {
       address: '0x964562f3377fbbf5acc39bffea89f8dc802a64ea' as Address,
-      abi: GoerliAbi,
+      abi: ContractAbi,
     }
   }
 
   if (provider && isMainnet(chain?.id)) {
     //TODO: Fix this
     return {
-      address: '0x964562f3377fbbf5acc39bffea89f8dc802a64ea' as Address,
-      abi: GoerliAbi,
+      address: '0x3a5846f85e7631c32b09528dd2479334a905ee42' as Address,
+      abi: ContractAbi,
     }
   }
 
   return {
     address: '0x964562f3377fbbf5acc39bffea89f8dc802a64ea' as Address,
-    abi: GoerliAbi,
+    abi: ContractAbi,
   }
 }
