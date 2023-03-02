d59b9ce0261ad84383cbeb74c72e0f63ea6f2220
Chris <chris.r.lee43@gmail.com>
2023-03-01 21:28:47 -0800

    add show more

--- components/niftyapes/FeaturedFinancingOffers.tsx
+++ components/niftyapes/FeaturedFinancingOffers.tsx
@@ -1,15 +1,24 @@
-import { Grid, Skeleton } from '@chakra-ui/react'
-import { useTokens } from '@reservoir0x/reservoir-kit-ui'
+import { Button, Grid, Skeleton, VStack } from '@chakra-ui/react'
+import { useCollections, useTokens } from '@reservoir0x/reservoir-kit-ui'
 import TokenCard from 'components/TokenCard'
 import useOffers from 'hooks/niftyapes/useOffers'
 import isEqualAddress from 'lib/niftyapes/isEqualAddress'
+import { uniq } from 'lodash'
+import { useState } from 'react'
 
 export default function FeaturedFinancingOffers() {
-  const { data: offersData, error, isLoading: isLoadingOffers } = useOffers({})
+  const [numOffers, setNumOffers] = useState(10) // Show 10 initial offers
+  const onShowMore = () => {
+    setNumOffers(numOffers + 10)
+  }
+  const { data: offersData, isLoading: isLoadingOffers } = useOffers({})
   const activeOffers = offersData?.filter((offer) => offer.status === 'ACTIVE')
-  const tokenQueries = activeOffers?.map(
-    (offer) => `${offer.offer.nftContractAddress}:${offer.offer.nftId}`
+  const tokenQueries = uniq(
+    activeOffers?.map(
+      (offer) => `${offer.offer.nftContractAddress}:${offer.offer.nftId}`
+    )
   )
+
   const {
     data: tokens,
     mutate: tokensMutate,
@@ -17,16 +26,6 @@ export default function FeaturedFinancingOffers() {
   } = useTokens({
     tokens: tokenQueries,
   })
-
-  if (isLoadingOffers || isFetchingTokens) {
-    return (
-      <Grid templateColumns={'repeat(5, 1fr)'} gap={12}>
-        {[...Array(10)].map((_, idx) => (
-          <Skeleton key={idx} rounded="md" height="sm" />
-        ))}
-      </Grid>
-    )
-  }
 
   const offersWithTokens = activeOffers
     ?.map((offer) => {
@@ -43,22 +42,66 @@ export default function FeaturedFinancingOffers() {
         token,
       }
     })
-    .filter(({ offer, token }) =>
-      // Filter out offers where creator is not the current NFT owner
-      isEqualAddress(offer.offer.creator, token?.token?.owner)
+    .filter(
+      ({ offer, token }) =>
+        // Filter out offers where creator is not the current NFT owner
+        token && isEqualAddress(offer.offer.creator, token?.token?.owner)
+    )
+    .slice(0, numOffers) // Only show numOffers
+
+  const collectionQueries = uniq(
+    offersWithTokens?.map(({ offer }) => offer.offer.nftContractAddress)
+  )
+
+  const { data: collections } = useCollections({
+    contract: collectionQueries,
+  })
+
+  const fullOffers = offersWithTokens?.map((offerWithToken) => {
+    const collection = collections?.find((collection) =>
+      isEqualAddress(
+        offerWithToken.offer.offer.nftContractAddress,
+        collection.id
+      )
     )
 
+    return {
+      ...offerWithToken,
+      collection,
+    }
+  })
+
+  if (isLoadingOffers || isFetchingTokens) {
+    return (
+      <Grid templateColumns={'repeat(5, 1fr)'} gap={12}>
+        {[...Array(numOffers)].map((_, idx) => (
+          <Skeleton key={idx} rounded="md" height="sm" />
+        ))}
+      </Grid>
+    )
+  }
+
   return (
-    <Grid templateColumns={'repeat(5, 1fr)'} gap={12}>
-      {offersWithTokens?.map(({ offer, token }, idx) => (
-        <TokenCard
-          key={idx}
-          token={token}
-          financeOffer={offer}
-          collectionImage={undefined}
-          mutate={tokensMutate}
-        ></TokenCard>
-      ))}
-    </Grid>
+    <VStack mb="12" spacing="12">
+      <Grid w="full" templateColumns={'repeat(5, 1fr)'} gap={12}>
+        {fullOffers?.map(({ offer, token, collection }, idx) => (
+          <TokenCard
+            key={idx}
+            token={token}
+            financeOffer={offer}
+            collection={collection}
+            collectionImage={collection?.image}
+            mutate={tokensMutate}
+          ></TokenCard>
+        ))}
+      </Grid>
+      <Button
+        hidden={numOffers > Number(fullOffers?.length)}
+        colorScheme={'purple'}
+        onClick={onShowMore}
+      >
+        Show more
+      </Button>
+    </VStack>
   )
 }
