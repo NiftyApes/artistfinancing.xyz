f507c52a5295bc481b4abfc09c20c4272212533c
Chris <chris.r.lee43@gmail.com>
2023-03-01 22:32:50 -0800

    Use Masonry Grid

--- components/niftyapes/FeaturedFinancingOffers.tsx
+++ components/niftyapes/FeaturedFinancingOffers.tsx
@@ -1,10 +1,11 @@
-import { Button, Grid, Skeleton, VStack } from '@chakra-ui/react'
+import { Box, Button, Skeleton, VStack } from '@chakra-ui/react'
 import { useCollections, useTokens } from '@reservoir0x/reservoir-kit-ui'
 import TokenCard from 'components/TokenCard'
 import useOffers from 'hooks/niftyapes/useOffers'
 import isEqualAddress from 'lib/niftyapes/isEqualAddress'
 import { uniq } from 'lodash'
 import { useState } from 'react'
+import Masonry from 'react-masonry-css'
 
 export default function FeaturedFinancingOffers() {
   const [numOffers, setNumOffers] = useState(10) // Show 10 initial offers
@@ -71,30 +72,40 @@ export default function FeaturedFinancingOffers() {
     }
   })
 
-  if (isLoadingOffers || isFetchingTokens) {
-    return (
-      <Grid templateColumns={'repeat(5, 1fr)'} gap={12}>
-        {[...Array(numOffers)].map((_, idx) => (
-          <Skeleton key={idx} rounded="md" height="sm" />
-        ))}
-      </Grid>
-    )
-  }
-
   return (
-    <VStack mb="12" spacing="12">
-      <Grid w="full" templateColumns={'repeat(5, 1fr)'} gap={12}>
-        {fullOffers?.map(({ offer, token, collection }, idx) => (
-          <TokenCard
-            key={idx}
-            token={token}
-            financeOffer={offer}
-            collection={collection}
-            collectionImage={collection?.image}
-            mutate={tokensMutate}
-          ></TokenCard>
-        ))}
-      </Grid>
+    <VStack mb="12" spacing="12" w="full" className="w-full">
+      <Masonry
+        key="tokensGridMasonry"
+        breakpointCols={{
+          default: 5,
+          1900: 5,
+          1536: 4,
+          1280: 3,
+          1024: 2,
+          768: 2,
+          640: 2,
+          500: 1,
+        }}
+        className="flex w-full gap-12"
+        columnClassName=""
+      >
+        {isLoadingOffers || isFetchingTokens
+          ? [...Array(numOffers)].map((_, idx) => (
+              <Skeleton key={idx} rounded="md" height="sm" mb="6" />
+            ))
+          : fullOffers?.map(({ offer, token, collection }, idx) => (
+              <Box mb="12">
+                <TokenCard
+                  key={idx}
+                  token={token}
+                  financeOffer={offer}
+                  collection={collection}
+                  collectionImage={collection?.image}
+                  mutate={tokensMutate}
+                />
+              </Box>
+            ))}
+      </Masonry>
       <Button
         hidden={numOffers > Number(fullOffers?.length)}
         colorScheme={'purple'}
