455bd1f5e44b18b0d191b3e6e1c72598e2edcb99
andreitr <andrei.taraschuk@gmail.com>
2023-03-15 16:55:25 -0600

    Formatting

--- hooks/niftyapes/useGoogleAnalytics.ts
+++ hooks/niftyapes/useGoogleAnalytics.ts
@@ -1,7 +1,6 @@
 import ReactGA from 'react-ga4'
 
 export const useGoogleAnalytics = () => {
-
   const trackEvent = (category: string, action: string, label: string) => {
     ReactGA.event({ category, action, label })
   }
