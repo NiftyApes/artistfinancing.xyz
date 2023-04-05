f571a9d49e834371ee036f138aa97cfa7973ccbf
andreitr <andrei.taraschuk@gmail.com>
2023-04-05 17:04:31 -0600

    Attributes and misc ui

--- components/token/TokenInfo.tsx
+++ components/token/TokenInfo.tsx
@@ -4,7 +4,6 @@ import React, { FC, useState } from 'react'
 import { FiExternalLink, FiRefreshCcw } from 'react-icons/fi'
 import { TokenDetails } from 'types/reservoir'
 import { setToast } from './setToast'
-import { mainnet } from 'wagmi/chains'
 
 const PROXY_API_BASE = process.env.NEXT_PUBLIC_PROXY_API_BASE
 
@@ -70,11 +69,9 @@ const TokenInfo: FC<Props> = ({ token }) => {
   }
 
   return (
-    <article className="col-span-full rounded-2xl border border-gray-300 bg-white p-6 dark:border-neutral-600 dark:bg-black">
+    <article className="col-span-full">
       <div className="mb-4 flex items-center justify-between">
-        <div className="reservoir-h5 font-headings dark:text-white">
-          Token Info
-        </div>
+        <div className='text-xs uppercase mb-2 text-gray-400'>details</div>
         <div className="flex items-center gap-2">
           <a
             className="reservoir-h6 font-headings"
@@ -120,35 +117,12 @@ const TokenInfo: FC<Props> = ({ token }) => {
           </div>
         </div>
       )}
-      <div className="mb-4 flex items-center justify-between">
-        <div className="reservoir-subtitle dark:text-white">Token ID</div>
-        <div className="reservoir-h6 max-w-[80px] truncate font-headings dark:text-white">
-          {token?.tokenId}
-        </div>
-      </div>
+
       <div className="mb-4 flex items-center justify-between">
         <div className="reservoir-subtitle dark:text-white">Token Standard</div>
         <div className="reservoir-h6 font-headings uppercase dark:text-white">
           {token?.kind}
         </div>
-      </div>
-      <div className="flex items-center justify-between">
-        <div className="reservoir-subtitle dark:text-white">
-          Metadata Refresh
-        </div>
-        <button
-          className="btn-primary-outline reservoir-h6 ml-auto flex items-center gap-2 p-2 font-headings text-primary-700 dark:border-neutral-600 dark:text-primary-100 dark:ring-primary-900 dark:focus:ring-4"
-          title="Refresh token"
-          disabled={refreshLoading}
-          onClick={() => refreshToken(`${token?.contract}:${token?.tokenId}`)}
-        >
-          Refresh{' '}
-          <FiRefreshCcw
-            className={`h-4 w-4 ${
-              refreshLoading ? 'animate-spin-reverse' : ''
-            }`}
-          />
-        </button>
       </div>
     </article>
   )
--- pages/[contract]/[tokenId]/index.tsx
+++ pages/[contract]/[tokenId]/index.tsx
@@ -2,7 +2,7 @@ import {
   useCollections,
   useTokenOpenseaBanned,
   useTokens,
-  useUserTokens,
+  useUserTokens
 } from '@reservoir0x/reservoir-kit-ui'
 import { paths } from '@reservoir0x/reservoir-sdk'
 import Layout from 'components/Layout'
@@ -51,32 +51,32 @@ const metadata = {
   title: (title: string) => (
     <>
       <title>{title}</title>
-      <meta property="twitter:title" content={title} />
-      <meta property="og:title" content={title} />
+      <meta property='twitter:title' content={title} />
+      <meta property='og:title' content={title} />
     </>
   ),
   description: (description: string) => (
     <>
-      <meta name="description" content={description} />
-      <meta name="twitter:description" content={description} />
-      <meta property="og:description" content={description} />
+      <meta name='description' content={description} />
+      <meta name='twitter:description' content={description} />
+      <meta property='og:description' content={description} />
     </>
   ),
   image: (image: string) => (
     <>
-      <meta name="twitter:image" content={image} />
-      <meta property="og:image" content={image} />
+      <meta name='twitter:image' content={image} />
+      <meta property='og:image' content={image} />
     </>
   ),
   tagline: (tagline: string | undefined) => (
     <>{tagline || 'Discover, buy and sell NFTs'}</>
-  ),
+  )
 }
 
 const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
   const [tokenOpenSea] = useState<any>({
     animation_url: null,
-    extension: null,
+    extension: null
   })
   const account = useAccount()
   const router = useRouter()
@@ -96,11 +96,11 @@ const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
 
   const tokenData = useTokens({
     tokens: [
-      `${router.query?.contract?.toString()}:${router.query?.tokenId?.toString()}`,
+      `${router.query?.contract?.toString()}:${router.query?.tokenId?.toString()}`
     ],
     includeTopBid: true,
     includeAttributes: true,
-    includeDynamicPricing: true,
+    includeDynamicPricing: true
   })
 
   const tokens = tokenData.data
@@ -111,8 +111,8 @@ const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
     checkUserOwnership ? account.address : undefined,
     {
       tokens: [
-        `${router.query?.contract?.toString()}:${router.query?.tokenId?.toString()}`,
-      ],
+        `${router.query?.contract?.toString()}:${router.query?.tokenId?.toString()}`
+      ]
     }
   )
 
@@ -128,10 +128,10 @@ const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
         const response = await data.json()
         fetch(`${PROXY_API_BASE}/seaport/offers`, {
           headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
           },
           method: 'POST',
-          body: JSON.stringify(response),
+          body: JSON.stringify(response)
         })
       })
     }
@@ -152,14 +152,14 @@ const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
   const description = META_DESCRIPTION
     ? metadata.description(META_DESCRIPTION)
     : token?.token?.description
-    ? metadata.description(token?.token?.description)
-    : null
+      ? metadata.description(token?.token?.description)
+      : null
 
   const image = META_OG_IMAGE
     ? metadata.image(META_OG_IMAGE)
     : token?.token?.image
-    ? metadata.image(token?.token?.image)
-    : null
+      ? metadata.image(token?.token?.image)
+      : null
 
   const isOwner =
     userTokens &&
@@ -176,16 +176,18 @@ const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
         {description}
         {image}
       </Head>
-      <div className="col-span-full content-start space-y-4 px-2 pt-4 md:col-span-4 lg:col-span-5 lg:col-start-2 lg:px-0 2xl:col-span-4 2xl:col-start-3 3xl:col-start-5 4xl:col-start-7">
-        <div className="mb-4">
+      <div
+        className='col-span-full content-start space-y-4 px-2 pt-4 md:col-span-4 lg:col-span-5 lg:col-start-2 lg:px-0 2xl:col-span-4 2xl:col-start-3 3xl:col-start-5 4xl:col-start-7'>
+        <div className='mb-4'>
           <TokenMedia token={token.token} />
         </div>
-        <div className="hidden space-y-4 md:block">
-          <CollectionInfo collection={collection} token={token.token} />
+          <TokenAttributes token={token?.token} />
+        <div className='hidden space-y-4 md:block'>
           <TokenInfo token={token.token} />
         </div>
       </div>
-      <div className="col-span-full mb-4 space-y-4 px-2 pt-0 md:col-span-4 md:col-start-5 md:pt-4 lg:col-span-5 lg:col-start-7 lg:px-0 2xl:col-span-5 2xl:col-start-7 3xl:col-start-9 4xl:col-start-11">
+      <div
+        className='col-span-full mb-4 space-y-4 px-2 pt-0 md:col-span-4 md:col-start-5 md:pt-4 lg:col-span-5 lg:col-start-7 lg:px-0 2xl:col-span-5 2xl:col-start-7 3xl:col-start-9 4xl:col-start-11'>
         <Owner
           details={token}
           bannedOnOpenSea={bannedOnOpenSea}
@@ -196,23 +198,10 @@ const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
           collection={collection}
           isOwner={isOwner}
         />
-        <PriceData
-          details={tokenData}
-          collection={collection}
-          isOwner={isOwner}
-        />
-        <TokenAttributes
-          token={token?.token}
-          collection={collection}
-          isOwner={isOwner}
-        />
-        {token.token?.kind === 'erc1155' && (
-          <Listings
-            token={`${router.query?.contract?.toString()}:${router.query?.tokenId?.toString()}`}
-          />
-        )}
       </div>
-      <div className="col-span-full block space-y-4 px-2 md:hidden lg:px-0">
+
+      <div className='col-span-full block space-y-4 px-2 md:hidden lg:px-0'>
+
         <CollectionInfo collection={collection} token={token.token} />
         <TokenInfo token={token.token} />
       </div>
@@ -225,7 +214,7 @@ export default Index
 export const getStaticPaths: GetStaticPaths = () => {
   return {
     paths: [],
-    fallback: 'blocking',
+    fallback: 'blocking'
   }
 }
 
@@ -245,7 +234,7 @@ export const getStaticProps: GetStaticProps<{
   ) {
     return {
       notFound: true,
-      revalidate: 10,
+      revalidate: 10
     }
   }
 
@@ -253,7 +242,7 @@ export const getStaticProps: GetStaticProps<{
 
   if (RESERVOIR_API_KEY) {
     options.headers = {
-      'x-api-key': RESERVOIR_API_KEY,
+      'x-api-key': RESERVOIR_API_KEY
     }
   }
 
@@ -264,7 +253,7 @@ export const getStaticProps: GetStaticProps<{
     includeTopBid: true,
     includeAttributes: true,
     includeDynamicPricing: true,
-    normalizeRoyalties: true,
+    normalizeRoyalties: true
   }
 
   const href = setParams(url, query)
@@ -279,11 +268,11 @@ export const getStaticProps: GetStaticProps<{
   if (!collectionId) {
     return {
       notFound: true,
-      revalidate: 10,
+      revalidate: 10
     }
   }
 
   return {
-    props: { collectionId, tokenDetails: data?.tokens?.[0]?.token },
+    props: { collectionId, tokenDetails: data?.tokens?.[0]?.token }
   }
 }
