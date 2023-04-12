2cbfc492cd7a5a193955fae49262989756ba24a4
andreitr <andrei.taraschuk@gmail.com>
2023-04-12 10:06:54 -0600

    Core review

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,50 +5,51 @@ const FOOTER_ENABLED = process.env.NEXT_PUBLIC_FOOTER_ENABLED == 'true'
 const Footer = () => {
   if (FOOTER_ENABLED)
     return (
-      <footer className="smm:flex-row col-span-full mt-20 flex flex-col justify-between bg-white pt-20 pb-20 text-black md:px-16">
-        <div className="mb-6 flex flex-row flex-wrap justify-between gap-x-6 text-xs sm:mb-0 sm:gap-x-8 sm:text-sm">
+      <footer
+        className='sm:flex-row col-span-full mt-20 flex flex-col justify-between bg-white pt-20 pb-20 text-black md:px-16'>
+        <div className='mb-6 flex flex-row flex-wrap justify-between gap-x-6 text-xs sm:mb-0 sm:gap-x-8 sm:text-sm'>
           <div>
-            <div className="text-xl font-bold">
+            <div className='text-xl font-bold'>
               SuperRare Underground by NiftyApes
             </div>
             <div>The future is rare bananas</div>
           </div>
 
           <div>
-            <div className="mb-1 font-bold	uppercase">Community</div>
+            <div className='mb-1 font-bold uppercase'>Community</div>
             <div>
-              <Link href="https://twitter.com/niftyapes" legacyBehavior={true}>
-                <a className="min-w-max" target="_blank" rel="noreferrer">
+              <Link href='https://twitter.com/niftyapes' legacyBehavior={true}>
+                <a className='min-w-max' target='_blank' rel='noreferrer'>
                   Twitter
                 </a>
               </Link>
             </div>
             <div>
               <Link
-                href="https://discord.com/invite/Ge8Zwy6syQ"
+                href='https://discord.com/invite/Ge8Zwy6syQ'
                 legacyBehavior={true}
               >
-                <a className="min-w-max" target="_blank" rel="noreferrer">
+                <a className='min-w-max' target='_blank' rel='noreferrer'>
                   Discord
                 </a>
               </Link>
             </div>
           </div>
           <div>
-            <div className="mb-1 font-bold	uppercase">Products</div>
+            <div className='mb-1 font-bold uppercase'>Products</div>
             <div>
               <Link
-                href="https://www.niftyapes.money/#top"
+                href='https://www.niftyapes.money/#top'
                 legacyBehavior={true}
               >
-                <a className="min-w-max" target="_blank" rel="noreferrer">
+                <a className='min-w-max' target='_blank' rel='noreferrer'>
                   SDK
                 </a>
               </Link>
             </div>
             <div>
-              <Link href="https://app.niftyapes.money/" legacyBehavior={true}>
-                <a className="min-w-max" target="_blank" rel="noreferrer">
+              <Link href='https://app.niftyapes.money/' legacyBehavior={true}>
+                <a className='min-w-max' target='_blank' rel='noreferrer'>
                   Lending App
                 </a>
               </Link>
