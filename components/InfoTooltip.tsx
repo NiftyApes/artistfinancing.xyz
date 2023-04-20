da17edb00ba52e916e93ad3b2e104b853e4d9aa6
Chris <chris.r.lee43@gmail.com>
2023-04-20 13:22:05 -0700

    Adjustments to tooltip

--- components/InfoTooltip.tsx
+++ components/InfoTooltip.tsx
@@ -11,17 +11,30 @@ const InfoTooltip = ({
   modeOverride?: 'light' | 'dark'
 }) => {
   let iconStyle = 'text-gray-500 dark:text-white'
+  let contentStyles = 'bg-black text-white dark:bg-white dark:text-black'
 
   if (modeOverride === 'light') {
     iconStyle = 'text-gray-500'
+    contentStyles = 'bg-black text-white'
   }
 
   if (modeOverride === 'dark') {
     iconStyle = 'text-white'
+    contentStyles = 'bg-white text-black'
   }
 
   return (
-    <Tooltip content={content} side={side} modeOverride={modeOverride}>
+    <Tooltip
+      content={
+        <div
+          className={`align-center p-2 text-center text-sm ${contentStyles}`}
+        >
+          {content}
+        </div>
+      }
+      side={side}
+      modeOverride={modeOverride}
+    >
       <div className={iconStyle}>
         <AiOutlineQuestionCircle />
       </div>
--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -4,7 +4,7 @@ import * as RadixTooltip from '@radix-ui/react-tooltip'
 export type Side = 'top' | 'bottom' | 'left' | 'right'
 
 interface Props extends RadixTooltip.TooltipProps {
-  content: string
+  content: React.ReactNode
   children: React.ReactNode
   side?: Side
   modeOverride?: 'light' | 'dark'
@@ -18,16 +18,13 @@ const Tooltip = ({
   ...props
 }: Props) => {
   let arrowStyles = 'fill-black dark:fill-white'
-  let contentStyles = 'bg-black text-white dark:bg-white dark:text-black'
 
   if (modeOverride === 'light') {
     arrowStyles = 'fill-black'
-    contentStyles = 'bg-black text-white'
   }
 
   if (modeOverride === 'dark') {
     arrowStyles = 'fill-white'
-    contentStyles = 'bg-white text-black'
   }
 
   return (
@@ -36,14 +33,15 @@ const Tooltip = ({
         <RadixTooltip.Trigger asChild>{children}</RadixTooltip.Trigger>
         <RadixTooltip.Content
           side={side}
+          sideOffset={5}
           className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade"
         >
-          <div
-            className={`align-center p-2 text-center text-sm ${contentStyles}`}
-          >
-            {content}
-          </div>
-          <RadixTooltip.Arrow className={`${arrowStyles}`} />
+          {content}
+          <RadixTooltip.Arrow
+            width={16}
+            height={8}
+            className={`${arrowStyles}`}
+          />
         </RadixTooltip.Content>
       </RadixTooltip.Root>
     </RadixTooltip.Provider>
