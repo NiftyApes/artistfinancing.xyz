71a9d138538c6f91d67b9444fdea4bf5d7bc92d6
Zach Herring <2172549+zherring@users.noreply.github.com>
2023-04-20 16:33:20 -0600

    super hacky re-layout

--- pages/[contract]/[tokenId]/index.tsx
+++ pages/[contract]/[tokenId]/index.tsx
@@ -16,7 +16,7 @@ import { useEffect } from 'react'
 import { TokenDetails } from 'types/reservoir'
 import { useAccount } from 'wagmi'
 import EthAccount from '../../../components/niftyapes/EthAccount'
-import Footer from '../../../components/Footer'
+// import Footer from '../../../components/Footer'
 import { optimizeImage } from '../../../lib/optmizeImage'
 import BuyNowPayLaterModal from '../../../components/niftyapes/bnpl/BuyNowPayLaterModal'
 
@@ -155,9 +155,9 @@ const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
 
   const renderBuyNowPayLater = () => {
     return (
-      <div className="max-w-100 rounded-lg border border-gray-600 p-5">
+      <div className="max-w-80 rounded-lg p-5 mb-10">
         <button
-          className={`flex h-[40px] w-full items-center justify-center whitespace-nowrap rounded-[21px] bg-white text-[14px] font-bold uppercase text-black focus:ring-0`}
+          className={`flex h-[50px] w-full items-center justify-center whitespace-nowrap rounded-[40px] bg-white text-[14px] font-bold uppercase text-black focus:ring-0`}
         >
           Buy Now, Pay Later
         </button>
@@ -172,17 +172,80 @@ const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
         {description}
         {image}
       </Head>
+      
+      {/* md:grid-cols-8 lg:grid-cols-12 3xl:grid-cols-16 4xl:grid-cols-21 */}
+      <div class="md:grid-span-8 lg:col-span-8 3xl:col-span-12 flex justify-center align-middle">
+              <img
+                alt={token?.token?.name || `#${token?.token?.tokenId}`}
+                // className="max-h-600"
+                src={optimizeImage(token?.token?.image, 533)}
+              />
+          {/* <div className="mb-14 flex justify-center align-middle">
+          </div> */}
+        </div>
 
-      <div className="col-span-full col-start-1 px-2 pt-4 md:col-span-full lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-2 2xl:col-span-10 2xl:col-start-2 3xl:col-start-4">
-        <div className="mb-14 flex justify-center">
+      <div className="col-span-8 md:col-span-4 relative md:height-[100vh] overflow-auto">
+      <div 
+          className="grid grid-flow-col gap-4"
+          >
+          <div className="col-span-3 resize-none">
+            <div className="reservoir-h3 mb-8 font-semibold">
+              {token?.token?.name || `#${token?.token?.tokenId}`}
+            </div>
+
+            <div className="mb-8 grid grid-flow-col grid-col-2 ">
+              <EthAccount
+                side="left"
+                label="Artist"
+                address={token?.token?.owner}
+                
+              />
+              <EthAccount
+                side="left"
+                label="Owner"
+                address={token?.token?.owner}
+              />
+            </div>
+
+            <div className="mb-14">
+              <div className="reservoir-h3 mb-1 font-semibold">Description</div>
+              <div className="text-md text-gray-300">
+                {token?.token?.description}
+              </div>
+            </div>
+
+            <div 
+              // className="mb-8 block lg:hidden"
+              className="ins-et-x-0 b-ottom-0 absolu-te"
+              >
+                {renderBuyNowPayLater()}
+            </div>
+
+            <div className="mb-14">
+              <TokenInfo token={token.token} />
+            </div>
+
+            <div className="mb-10">
+              <TokenAttributes token={token?.token} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div 
+        className="col-span-full col-start-1 px-2 pt-4 md:col-s-pan-full lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-2 2xl:col-span-10 2xl:col-start-2 3xl:col-start-4"
+        >
+        {/* <div className="mb-14 flex justify-center">
           <img
             alt={token?.token?.name || `#${token?.token?.tokenId}`}
             className="max-h-600"
             src={optimizeImage(token?.token?.image, 533)}
           />
-        </div>
+        </div> */}
 
-        <div className="grid grid-flow-col gap-4">
+        {/* <div 
+          className="grid grid-flow-col gap-4"
+          >
           <div className="col-span-3 resize-none">
             <div className="reservoir-h3 mb-8 font-semibold">
               {token?.token?.name || `#${token?.token?.tokenId}`}
@@ -222,10 +285,10 @@ const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
           <div className="col-span-1 hidden lg:block">
             {renderBuyNowPayLater()}
           </div>
-        </div>
+        </div> */}
       </div>
 
-      <Footer />
+      {/* <Footer /> */}
     </Layout>
   )
 }
