e7a1382bb9ecc9b5198fd3c33b3517de0999ef9e
andreitr <andrei.taraschuk@gmail.com>
2023-05-02 19:18:10 -0600

    make payment wip

--- components/niftyapes/MakePaymentModal.tsx
+++ components/niftyapes/MakePaymentModal.tsx
@@ -12,14 +12,13 @@ import {
   ModalOverlay,
   Text,
   VStack,
-  useDisclosure,
+  useDisclosure
 } from '@chakra-ui/react'
 import { format } from 'date-fns'
 import { AiOutlineArrowRight } from 'react-icons/ai'
 import { SiEthereum } from 'react-icons/si'
 import PaymentModalTermStats from './PaymentModalTermStats'
 import { BigNumber } from 'ethers'
-import { useMakePayment } from '../../hooks/niftyapes/useMakePayment'
 import { OfferDetails } from '@niftyapes/sdk'
 import { useEtherscanUri } from '../../hooks/niftyapes/useEtherscan'
 import { processOffer } from '../../lib/niftyapes/processOffer'
@@ -29,12 +28,14 @@ import { useEffect } from 'react'
 import { useQueryClient } from 'react-query'
 import { useRouter } from 'next/router'
 import { LoanDetails } from 'hooks/niftyapes/useLoans'
+import { useMakePayment } from '@niftyapes/sdk'
+import { useWaitForTransaction } from 'wagmi'
 
 export default function MakePaymentModal({
-  data,
-  loan,
-  offer,
-}: {
+                                           data,
+                                           loan,
+                                           offer
+                                         }: {
   data: Record<string, any>
   loan: LoanDetails
   offer: OfferDetails
@@ -64,107 +65,109 @@ export default function MakePaymentModal({
     duration: loanDurMos,
     minPayment: payment,
     payPeriodDays: payPeriodDays,
-    remainingPrincipal: loan.remainingPrincipal,
+    remainingPrincipal: loan.remainingPrincipal
   }
 
   const {
-    data: paymentTransaction,
-    isLoading,
-    isSuccess,
+    error,
+    data: paymentTxn,
     isError,
-    write,
+    status,
+    write
   } = useMakePayment({
     nftContractAddress: offer.nftContractAddress,
     nftId: BigNumber.from(offer.nftId),
-    paymentAmount: payment,
+    paymentAmount: payment
+  })
+
+  console.log('Status here...', status)
+
+  const { isLoading: isLoadingTxn, isSuccess: isSuccessTxn, isError: isErrorTxn } = useWaitForTransaction({
+    hash: paymentTxn?.hash
   })
 
   useEffect(() => {
     // Invalidates parent table query three seconds after the transaction
-    if (isSuccess) {
+    if (isSuccessTxn) {
       setTimeout(() => {
         queryClient.invalidateQueries({ queryKey: [parentTableQuery] })
       }, 3000)
     }
-  }, [isSuccess])
+  }, [isSuccessTxn])
+
 
   return (
     <>
       <button
         onClick={onOpen}
-        className="btn-primary-fill gap-2 dark:ring-primary-900 dark:focus:ring-4"
+        className='btn-primary-fill gap-2 dark:ring-primary-900 dark:focus:ring-4'
       >
         Make Payment
       </button>
-      <Modal isOpen={isOpen} onClose={onClose} size="4xl">
+      <Modal isOpen={isOpen} onClose={onClose} size='4xl'>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader borderTopRadius="md" bg="gray.700">
+          <ModalHeader borderTopRadius='md' bg='gray.700'>
             Make Payment
           </ModalHeader>
           <ModalCloseButton />
-          <ModalBody p="0">
+          <ModalBody p='0'>
             <Flex>
               <VStack
-                w="72"
-                borderRight="1px"
-                borderColor="gray.600"
-                p="6"
-                align="left"
+                w='72'
+                borderRight='1px'
+                borderColor='gray.600'
+                p='6'
+                align='left'
                 flexShrink={0}
               >
                 <Image
-                  borderRadius="md"
-                  boxSize="200px"
-                  objectFit="cover"
+                  borderRadius='md'
+                  boxSize='200px'
+                  objectFit='cover'
                   src={data.image}
                   alt={data.image}
                 ></Image>
-                <VStack align="left" w="full">
-                  <Heading size="sm">{data.name}</Heading>
-                  <Text mt="0 !important" fontSize="xs" color="whiteAlpha.800">
+                <VStack align='left' w='full'>
+                  <Heading size='sm'>{data.name}</Heading>
+                  <Text mt='0 !important' fontSize='xs' color='whiteAlpha.800'>
                     {data.collectionName}
                   </Text>
                 </VStack>
                 <PaymentModalTermStats terms={terms} />
               </VStack>
-              <Box p="6" w="full">
-                {paymentTransaction &&
-                  isSuccess &&
-                  `Thanks for your payment. Transaction processed successfully. `}
-                {paymentTransaction &&
-                  isLoading &&
-                  `Processing transaction... `}
-                {paymentTransaction &&
-                  isError &&
-                  `Unable to process transaction... `}
-                {paymentTransaction && (
+              <Box p='6' w='full'>
+
+                {isError && 'We have an error here...'}
+
+                {paymentTxn &&
                   <Box>
-                    <Link
-                      href={`${etherscanUri}/tx/${paymentTransaction.hash}`}
-                      isExternal
-                    >
-                      View Transaction
-                    </Link>
+                    {isSuccessTxn && `Thanks for your payment. Transaction processed successfully. `}
+                    {isLoadingTxn && `Processing transaction... `}
+                    {isErrorTxn && `Unable to process transaction... `}
+
+                    <Link href={`${etherscanUri}/tx/${paymentTxn?.hash}`} isExternal>View Transaction</Link>
                   </Box>
-                )}
-                {!paymentTransaction && (
+                }
+
+
+                {!paymentTxn && (
                   <VStack
-                    align="start"
-                    width="full"
-                    justify="space-between"
+                    align='start'
+                    width='full'
+                    justify='space-between'
                     gap={4}
                   >
-                    <div className="flex w-full flex-row items-center justify-between rounded-md bg-gray-700 p-4">
-                      <div className="flex flex-col items-center gap-2">
-                        <div className="flex flex-row items-center gap-2 text-xl font-semibold">
-                          <p>
-                            {Number(
-                              formatEther(loan.remainingPrincipal)
-                            ).toFixed(4)}
-                          </p>
+                    <div className='flex w-full flex-row items-center justify-between rounded-md bg-gray-700 p-4'>
+                      <div className='flex flex-col items-center gap-2'>
+                        <div className='flex flex-row items-center gap-2 text-xl font-semibold'>
+
+                          {Number(
+                            formatEther(loan.remainingPrincipal)
+                          ).toFixed(4)}
+
                           <AiOutlineArrowRight />
-                          <p className="text-green-400">
+                          <span className='text-green-400'>
                             {Number(
                               formatEther(
                                 BigNumber.from(loan.remainingPrincipal).sub(
@@ -172,25 +175,23 @@ export default function MakePaymentModal({
                                 )
                               )
                             ).toFixed(4)}
-                          </p>
+                          </span>
                         </div>
-                        <p className="text-sm text-gray-300">
+                        <p className='text-sm text-gray-300'>
                           Principal Change
                         </p>
                       </div>
-                      <div className="flex flex-col items-center gap-2">
-                        <div className="flex flex-row items-center gap-2 text-xl font-semibold">
-                          <p>
-                            <FormatNativeCrypto
-                              maximumFractionDigits={4}
-                              amount={payment}
-                            />
-                          </p>
+                      <div className='flex flex-col items-center gap-2'>
+                        <div className='flex flex-row items-center gap-2 text-xl font-semibold'>
+                          <FormatNativeCrypto
+                            maximumFractionDigits={4}
+                            amount={payment}
+                          />
                         </div>
-                        <p className="text-sm text-gray-300">Payment Due Now</p>
+                        <p className='text-sm text-gray-300'>Payment Due Now</p>
                       </div>
-                      <div className="flex flex-col items-center gap-2">
-                        <div className="flex flex-row items-center font-semibold">
+                      <div className='flex flex-col items-center gap-2'>
+                        <div className='flex flex-row items-center font-semibold'>
                           <p>
                             {format(
                               new Date(loan.periodEndTimestamp * 1000),
@@ -198,26 +199,30 @@ export default function MakePaymentModal({
                             )}
                           </p>
                         </div>
-                        <p className="text-sm text-gray-300">
+                        <p className='text-sm text-gray-300'>
                           Next Payment Due
                         </p>
                       </div>
                     </div>
 
-                    <div className="flex w-full flex-row items-center justify-between rounded-md bg-gray-700 p-4">
-                      <div className="flex w-full flex-row items-center gap-4">
-                        <div className="flex flex-row items-center gap-2 text-2xl font-bold">
+                    <div className='flex w-full flex-row items-center justify-between rounded-md bg-gray-700 p-4'>
+                      <div className='flex w-full flex-row items-center gap-4'>
+                        <div className='flex flex-row items-center gap-2 text-2xl font-bold'>
                           <SiEthereum />
                           <p>ETH</p>
                         </div>
                         <input
+                          readOnly={true}
                           value={formatEther(payment)}
-                          type="number"
-                          className="reservoir-label-l input-primary-outline dark:border-neutral-600 dark:bg-neutral-800 dark:text-white dark:ring-primary-900 dark:placeholder:text-neutral-400  dark:focus:ring-4"
+                          type='number'
+                          className='reservoir-label-l input-primary-outline dark:border-neutral-600 dark:bg-neutral-800 dark:text-white dark:ring-primary-900 dark:placeholder:text-neutral-400  dark:focus:ring-4'
                         />
                         <button
-                          className="btn-purple-fill ml-auto"
-                          onClick={() => write?.()}
+                          className='btn-purple-fill ml-auto'
+                          onClick={() => {
+                            console.log('Clicked....')
+                            write?.()
+                          }}
                         >
                           Make Payment
                         </button>
