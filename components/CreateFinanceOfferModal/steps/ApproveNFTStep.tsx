5e356243688b5aecc90cffd5ea34eb81782218c3
Chris <chris.r.lee43@gmail.com>
2023-05-17 13:20:02 -0700

    update

--- components/CreateFinanceOfferModal/steps/ApproveNFTStep.tsx
+++ components/CreateFinanceOfferModal/steps/ApproveNFTStep.tsx
@@ -1,34 +1,35 @@
-import { useERC721Approve, Address } from '@niftyapes/sdk'
+import { Address, useERC721Approve } from '@niftyapes/sdk'
 import { useTokens } from '@reservoir0x/reservoir-kit-ui'
-import { FC } from 'react'
-import { useNetwork } from 'wagmi'
+import { FC, useEffect } from 'react'
 
 type Props = {
   token: ReturnType<typeof useTokens>['data'][0]
 }
 
 const ApproveNFTStep: FC<Props> = ({ token }) => {
-  console.log(token?.token?.tokenId)
-
-  const { chain, chains } = useNetwork()
-  console.log(chain, chains)
-
+  // TODO: Errors
   const { approvalRequired, approvalCheckErr, write } = useERC721Approve({
     tokenId: token?.token?.tokenId as string,
     contractAddress: token?.token?.contract as Address,
   })
 
+  useEffect(() => {
+    console.log('write', write)
+    write?.()
+  }, [])
+
   return (
     <div className="flex flex-col space-y-1">
       <p className="text-sm text-black">Approve NiftyApes to access your NFT</p>
       {approvalRequired && (
-        <p className="text-xs underline">Approval transaction required</p>
+        <p className="text-xs underline">Waiting for approval</p>
       )}
       {approvalCheckErr && (
         <p className="text-xs text-red-500 underline">
           Error checking approval
         </p>
       )}
+      {write && <button onClick={write}>Write</button>}
     </div>
   )
 }
