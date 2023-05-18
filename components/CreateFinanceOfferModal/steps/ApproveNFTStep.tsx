f7f719667d4bc54025609ab30bc4abab8fb6f7e5
Chris <chris.r.lee43@gmail.com>
2023-05-18 11:46:14 -0700

    implement approve hook

--- components/CreateFinanceOfferModal/steps/ApproveNFTStep.tsx
+++ components/CreateFinanceOfferModal/steps/ApproveNFTStep.tsx
@@ -1,35 +1,80 @@
 import { Address, useERC721Approve } from '@niftyapes/sdk'
 import { useTokens } from '@reservoir0x/reservoir-kit-ui'
 import { FC, useEffect } from 'react'
+import { IoReload } from 'react-icons/io5'
+import { BeatLoader } from 'react-spinners'
+import { useWaitForTransaction } from 'wagmi'
 
 type Props = {
   token: ReturnType<typeof useTokens>['data'][0]
 }
 
 const ApproveNFTStep: FC<Props> = ({ token }) => {
-  // TODO: Errors
-  const { approvalRequired, approvalCheckErr, write } = useERC721Approve({
+  const {
+    approvalRequired,
+    approvalCheckErr,
+    data,
+    write,
+    isLoading,
+    isError,
+  } = useERC721Approve({
     tokenId: token?.token?.tokenId as string,
     contractAddress: token?.token?.contract as Address,
   })
 
+  const {
+    isLoading: isTxLoading,
+    isSuccess: isTxSuccess,
+    isError: isTxError,
+  } = useWaitForTransaction({ hash: data?.hash })
+
   useEffect(() => {
-    console.log('write', write)
-    write?.()
-  }, [])
+    if (approvalRequired) {
+      write?.()
+    }
+  }, [approvalRequired])
 
   return (
     <div className="flex flex-col space-y-1">
       <p className="text-sm text-black">Approve NiftyApes to access your NFT</p>
-      {approvalRequired && (
-        <p className="text-xs underline">Waiting for approval</p>
-      )}
-      {approvalCheckErr && (
-        <p className="text-xs text-red-500 underline">
-          Error checking approval
-        </p>
-      )}
-      {write && <button onClick={write}>Write</button>}
+      <div className="flex items-center space-x-2">
+        {approvalCheckErr && (
+          <p className="text-xs text-red-500 underline">
+            Error checking approval
+          </p>
+        )}
+        {approvalRequired && isLoading && (
+          <>
+            <p className="text-xs underline">Waiting for approval</p>
+            <BeatLoader size={5} color="#36d7b7" />
+          </>
+        )}
+        {!approvalRequired && (
+          <p className="text-xs underline">NiftyApes approved to access</p>
+        )}
+        {isError && (
+          <>
+            <p className="text-xs text-red-500 underline">
+              Error granting approval
+            </p>
+            <button onClick={() => write?.()}>
+              <IoReload />
+            </button>
+          </>
+        )}
+        {isTxLoading && (
+          <>
+            <p className="text-xs underline">Waiting transaction</p>
+            <BeatLoader size={5} color="#36d7b7" />
+          </>
+        )}
+        {isTxSuccess && (
+          <p className="text-xs underline">Transaction complete</p>
+        )}
+        {isTxError && (
+          <p className="text-xs text-red-500 underline">Transaction error</p>
+        )}
+      </div>
     </div>
   )
 }
