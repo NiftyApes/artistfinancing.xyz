dc301261a80a849e35425c10efedb38e0a4c6d9f
andreitr <andrei.taraschuk@gmail.com>
2023-06-27 14:57:46 -0600

    Implemented explore link in the header

--- components/navbar/NavbarLogo.tsx
+++ components/navbar/NavbarLogo.tsx
@@ -5,7 +5,6 @@ import { FC } from 'react'
 const NAVBAR_LOGO = process.env.NEXT_PUBLIC_NAVBAR_LOGO
 const SOURCE_ID = process.env.NEXT_PUBLIC_SOURCE_ID
 const SOURCE_NAME = process.env.NEXT_PUBLIC_SOURCE_NAME
-const DESKTOP_NAVBAR_LOGO = process.env.NEXT_PUBLIC_DESKTOP_NAVBAR_LOGO
 const NAVBAR_LOGO_LINK = process.env.NEXT_PUBLIC_NAVBAR_LOGO_LINK
 
 type Props = {
@@ -15,8 +14,6 @@ type Props = {
 
 const NavbarLogo: FC<Props> = ({ variant, className }) => {
   const logo = NAVBAR_LOGO || '/reservoir.svg'
-  const desktopLogo = DESKTOP_NAVBAR_LOGO || '/reservoir-desktop.svg'
-  const chain = useEnvChain()
   let logoAlt = 'Logo'
 
   if (SOURCE_NAME) {
@@ -26,27 +23,16 @@ const NavbarLogo: FC<Props> = ({ variant, className }) => {
   }
 
   const mobileVariant = variant == 'mobile'
-  const desktopVariant = variant == 'desktop'
 
   return (
     <Link href={NAVBAR_LOGO_LINK || '/'} legacyBehavior={true}>
       <a
         className={`relative inline-flex flex-none items-center gap-1 ${className}`}
       >
-        <img
-          src={logo}
-          alt={logoAlt}
-          className={`h-6 w-auto ${!variant ? 'md:hidden' : ''} ${
-            desktopVariant ? 'hidden' : ''
-          } ${mobileVariant ? 'block' : ''}`}
-        />
-        <img
-          src={desktopLogo}
-          alt={logoAlt}
-          className={`h-6 w-auto md:block ${
-            !variant ? 'hidden md:block' : ''
-          } ${mobileVariant ? 'hidden' : ''} ${desktopVariant ? 'block' : ''}`}
-        />
+        <img src={logo} alt={logoAlt} className={`mr-5 block h-6 w-auto`} />
+        <span className="text-gray-400 hover:text-white hover:underline">
+          Explore
+        </span>
       </a>
     </Link>
   )
