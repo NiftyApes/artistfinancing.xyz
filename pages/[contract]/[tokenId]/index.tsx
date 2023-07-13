8820e43fcc616def299bdbcc19099a43741d10a4
andreitr <andrei.taraschuk@gmail.com>
2023-07-13 17:40:28 -0600

    WIP - displaying listings

--- pages/[contract]/[tokenId]/index.tsx
+++ pages/[contract]/[tokenId]/index.tsx
@@ -13,6 +13,11 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { TokenDetails } from 'types/reservoir'
 import { useAccount } from 'wagmi'
+import { useOffers } from '@niftyapes/sdk'
+import { AiFillTags, AiOutlineRightCircle } from 'react-icons/all'
+import FormatCurrency from '../../../components/FormatCurrency'
+import { DateTime, Duration } from 'luxon'
+import { formatBN } from '../../../lib/numbers'
 
 // Environment variables
 // For more information about these variables
@@ -42,26 +47,23 @@ const metadata = {
   title: (title: string) => (
     <>
       <title>{title}</title>
-      <meta property="twitter:title" content={title} />
-      <meta property="og:title" content={title} />
+      <meta property='twitter:title' content={title} />
+      <meta property='og:title' content={title} />
     </>
   ),
   description: (description: string) => (
     <>
-      <meta name="description" content={description} />
-      <meta name="twitter:description" content={description} />
-      <meta property="og:description" content={description} />
+      <meta name='description' content={description} />
+      <meta name='twitter:description' content={description} />
+      <meta property='og:description' content={description} />
     </>
   ),
   image: (image: string) => (
     <>
-      <meta name="twitter:image" content={image} />
-      <meta property="og:image" content={image} />
+      <meta name='twitter:image' content={image} />
+      <meta property='og:image' content={image} />
     </>
-  ),
-  tagline: (tagline: string | undefined) => (
-    <>{tagline || 'Discover, buy and sell NFTs'}</>
-  ),
+  )
 }
 
 const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
@@ -70,11 +72,11 @@ const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
 
   const tokenData = useTokens({
     tokens: [
-      `${router.query?.contract?.toString()}:${router.query?.tokenId?.toString()}`,
+      `${router.query?.contract?.toString()}:${router.query?.tokenId?.toString()}`
     ],
     includeTopBid: true,
     includeAttributes: true,
-    includeDynamicPricing: true,
+    includeDynamicPricing: true
   })
 
   const tokens = tokenData.data
@@ -85,8 +87,8 @@ const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
     checkUserOwnership ? account.address : undefined,
     {
       tokens: [
-        `${router.query?.contract?.toString()}:${router.query?.tokenId?.toString()}`,
-      ],
+        `${router.query?.contract?.toString()}:${router.query?.tokenId?.toString()}`
+      ]
     }
   )
 
@@ -102,14 +104,22 @@ const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
         const response = await data.json()
         fetch(`${PROXY_API_BASE}/seaport/offers`, {
           headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
           },
           method: 'POST',
-          body: JSON.stringify(response),
+          body: JSON.stringify(response)
         })
       })
     }
   }, [])
+
+  const offers = useOffers({
+    collection: token?.token?.contract!,
+    nftId: token?.token?.tokenId!
+  })
+
+  const activeOffers = offers.data?.filter((offer) => offer.status === 'ACTIVE') || []
+  const hasActiveOffers = activeOffers && activeOffers.length > 0
 
   if (tokenData.error) {
     return <div>There was an error</div>
@@ -126,14 +136,14 @@ const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
   const description = META_DESCRIPTION
     ? metadata.description(META_DESCRIPTION)
     : token?.token?.description
-    ? metadata.description(token?.token?.description)
-    : null
+      ? metadata.description(token?.token?.description)
+      : null
 
   const image = META_OG_IMAGE
     ? metadata.image(META_OG_IMAGE)
     : token?.token?.image
-    ? metadata.image(token?.token?.image)
-    : null
+      ? metadata.image(token?.token?.image)
+      : null
 
   const isOwner =
     userTokens &&
@@ -150,56 +160,93 @@ const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
         {description}
         {image}
       </Head>
-      <div className="col-span-full lg:col-span-8 lg:pr-12 3xl:col-span-12">
-        <div className="flex items-center justify-center p-4 lg:h-vh-minus-6rem">
+      <div className='col-span-full lg:col-span-8 lg:pr-12 3xl:col-span-12'>
+        <div className='flex items-center justify-center p-4 lg:h-vh-minus-6rem'>
           <img
             alt={token?.token?.name || `#${token?.token?.tokenId}`}
-            className="lg:max-h-xl object-cover lg:max-w-xl"
+            className='lg:max-h-xl object-cover lg:max-w-xl'
             src={optimizeImage(token?.token?.image, 533)}
           />
         </div>
       </div>
 
-      <div className="relative col-span-full flex overflow-auto lg:col-span-4 lg:h-vh-minus-6rem lg:pr-12">
-        <div className="grid w-full grid-flow-col gap-4 lg:w-auto">
-          <div className="resize-none lg:col-span-3">
-            <div className="reservoir-h3 mb-8 text-center font-semibold lg:text-left">
+      <div className='relative col-span-full flex overflow-auto lg:col-span-4 lg:h-vh-minus-6rem lg:pr-12'>
+        <div className='grid w-full grid-flow-col gap-4 lg:w-auto'>
+          <div className='resize-none lg:col-span-3'>
+            <div className='reservoir-h3 mb-8 text-center font-semibold lg:text-left'>
               {token?.token?.name || `#${token?.token?.tokenId}`}
             </div>
 
-            <div className="mb-8 flex items-center justify-center space-x-[100px] lg:justify-start">
+            <div className='mb-8 flex items-center justify-center space-x-[100px] lg:justify-start'>
               <EthAccount
-                side="left"
-                label="Artist"
+                side='left'
+                label='Artist'
                 address={token?.token?.owner}
               />
               <EthAccount
-                side="left"
-                label="Owner"
+                side='left'
+                label='Owner'
                 address={token?.token?.owner}
               />
             </div>
 
-            <div className="flex items-center justify-center lg:justify-start">
-              <div className="w-[460px] lg:w-auto">
-                <div className="mb-10">
+            <div className='flex items-center justify-center lg:justify-start'>
+              <div className='w-[460px] lg:w-auto'>
+                <div className='mb-10'>
                   <OfferSection token={token} isOwner={isOwner} />
                 </div>
 
-                <div className="mb-10">
-                  <div className="reservoir-h3 mb-1 flex font-semibold">
+
+                {(hasActiveOffers && isOwner) &&
+                  <div className='mb-10'>
+                    <div className='flex items-center h-full'>
+                      <div className='reservoir-h3 mb-1 flex font-semibold mr-5'>
+                        Listings
+                      </div>
+                      <AiFillTags className='text-xs text-gray-500 mr-2' />
+                      <div className='text-xs text-gray-500'>{`${activeOffers.length} Active Listing${
+                        activeOffers.length === 1 ? '' : 's'
+                      }`}</div>
+                    </div>
+                    <div className='text-xs text-gray-400 mt-5'>
+                      {activeOffers.map((offer) => {
+                        return <div className='flex items-center h-full mb-4 mt-2'>
+                          <AiOutlineRightCircle className='text-xl text-gray-600 mr-3' />
+                          <div>
+                            {`${formatBN(offer.offer.price, 2) as any} ETH over ${Duration.fromObject({
+                              seconds: offer.offer.periodDuration
+                            }).as('days')} days`}
+                          </div>
+
+                          {offer.status.toLowerCase() === 'active' &&
+                            <div className='border border-gray-600 border-opacity-50 rounded-lg px-2 py-[2px] ml-auto'>
+                              expires {DateTime.fromSeconds(
+                              offer.offer.expiration
+                            ).toRelative()!}
+                            </div>
+                          }
+                        </div>
+                      })
+                      }
+                    </div>
+                  </div>
+                }
+
+
+                <div className='mb-10'>
+                  <div className='reservoir-h3 mb-1 flex font-semibold'>
                     Description
                   </div>
-                  <div className="text-md flex text-gray-300">
+                  <div className='text-md flex text-gray-300'>
                     {token?.token?.description}
                   </div>
                 </div>
 
-                <div className="mb-14">
+                <div className='mb-14'>
                   <TokenInfo token={token.token} />
                 </div>
 
-                <div className="mb-10">
+                <div className='mb-10'>
                   <TokenAttributes token={token?.token} />
                 </div>
               </div>
@@ -216,7 +263,7 @@ export default Index
 export const getStaticPaths: GetStaticPaths = () => {
   return {
     paths: [],
-    fallback: 'blocking',
+    fallback: 'blocking'
   }
 }
 
@@ -236,7 +283,7 @@ export const getStaticProps: GetStaticProps<{
   ) {
     return {
       notFound: true,
-      revalidate: 10,
+      revalidate: 10
     }
   }
 
@@ -244,7 +291,7 @@ export const getStaticProps: GetStaticProps<{
 
   if (RESERVOIR_API_KEY) {
     options.headers = {
-      'x-api-key': RESERVOIR_API_KEY,
+      'x-api-key': RESERVOIR_API_KEY
     }
   }
 
@@ -255,7 +302,7 @@ export const getStaticProps: GetStaticProps<{
     includeTopBid: true,
     includeAttributes: true,
     includeDynamicPricing: true,
-    normalizeRoyalties: true,
+    normalizeRoyalties: true
   }
 
   const href = setParams(url, query)
@@ -270,11 +317,11 @@ export const getStaticProps: GetStaticProps<{
   if (!collectionId) {
     return {
       notFound: true,
-      revalidate: 10,
+      revalidate: 10
     }
   }
 
   return {
-    props: { collectionId, tokenDetails: data?.tokens?.[0]?.token },
+    props: { collectionId, tokenDetails: data?.tokens?.[0]?.token }
   }
 }
