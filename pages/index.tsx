f5dd07f7023b2b101aa176f9de56ed225eaa7857
andreitr <andrei.taraschuk@gmail.com>
2023-08-03 12:11:05 -0600

    Featured art grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ import { AiOutlinePlus } from 'react-icons/ai'
 import { BsArrowRight } from 'react-icons/bs'
 import Link from 'next/link'
 import { useAccount } from 'wagmi'
+import HomeGrid from '../components/HomeGrid'
 
 // Environment variables
 // For more information about these variables
@@ -78,6 +79,53 @@ const CAROUSEL = [
       'https://ipfs.pixura.io/ipfs/QmaYrjB5XrgHpVsCyLvsRN6kCKmyP6ZBbTvAM7aiK56p37/quantumania.jpg',
     rarity: '1/1',
     title: 'Quantumania'
+  }
+]
+
+const FEATURED = [
+  {
+    artist: 'Miss AL Simpson',
+    buyFinancingPrice: 4.12,
+    buyNowPrice: 20.6,
+    image:
+      'https://pixura.imgix.net/https%3A%2F%2Fstorage.googleapis.com%2Fsr_prod_artworks_bucket%2F0x816445f9a8359ef8405e79ad6041584a9e157d36%252F4%252F2bbf5a60-54a8-464a-8706-0fa0a6ec90d8%252Furi%252Fimage-2023-08-02-15-02-u89kmk?ixlib=js-3.8.0&h=3000&fit=clip&q=100&auto=format&s=48abab3f11655d893a43d2f960a9a6fb',
+    rarity: '1/1',
+    title: 'HAUNTED BRICKS'
+  },
+  {
+    artist: 'Miss AL Simpson',
+    buyFinancingPrice: 4.12,
+    buyNowPrice: 20.6,
+    image:
+      'https://pixura.imgix.net/https%3A%2F%2Fstorage.googleapis.com%2Fsr_prod_artworks_bucket%2F0x8a1521398214a61bef23e0a2f2909a6a152f9341%252F182%252F7da39fb1-a2ec-40b6-8c78-2dda40e3bcc7%252Furi%252Fimage-2023-08-02-10-56-vrycn?ixlib=js-3.8.0&w=3000&fit=clip&q=100&auto=format&s=eeb1097733881fd47ffb75bd9d966453',
+    rarity: '1/1',
+    title: 'HAUNTED BRICKS'
+  },
+  {
+    artist: 'Miss AL Simpson',
+    buyFinancingPrice: 4.12,
+    buyNowPrice: 20.6,
+    image:
+      'https://pixura.imgix.net/https%3A%2F%2Fstorage.googleapis.com%2Fsr_prod_artworks_bucket%2F0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0%252F45620%252F9f5cf41c-6869-40c9-b286-acf042bac3d5%252Furi%252Fimage-2023-07-27-00-44-xfyag?ixlib=js-3.8.0&w=1080&fit=clip&q=100&auto=format&s=bbe92020ff2c95e50409189908f3c10a',
+    rarity: '1/1',
+    title: 'HAUNTED BRICKS'
+  },
+  {
+    artist: 'Miss AL Simpson',
+    buyFinancingPrice: 4.12,
+    buyNowPrice: 20.6,
+    image:
+      'https://pixura.imgix.net/https%3A%2F%2Fstorage.googleapis.com%2Fsr_prod_artworks_bucket%2F0x8ab2798533803da9563bcd716e868c33cc323609%252F4%252Fc89ca6f4-cd75-4025-8a72-e315f558c35b%252Furi%252Fimage-2023-08-02-17-12-1ln7t?ixlib=js-3.8.0&w=550&h=550&fit=crop&q=75&auto=format%2Ccompress&s=fc17a92c78ca07bf8aab2fd2eb86f440',
+    rarity: '1/1',
+    title: 'HAUNTED BRICKS'
+  }, {
+    artist: 'Miss AL Simpson',
+    buyFinancingPrice: 4.12,
+    buyNowPrice: 20.6,
+    image:
+      'https://ipfs.pixura.io/ipfs/QmWF8CamCB9Ar5i8F5J7qQGa8PVF281PBo1FUgNHHHtgBY/THETHINKER.JPG',
+    rarity: '1/1',
+    title: 'HAUNTED BRICKS'
   }
 ]
 
@@ -204,7 +252,7 @@ const Home: NextPage<Props> = ({ fallback }) => {
         <div className='col-span-full mt-[90px] mb-[90px] w-full border-t border-gray-700'></div>
         <div className='text-6xl'>Everyone Benefits</div>
 
-        <div className='mt-[60px] mb-[90px] flex items-center'>
+        <div className='mt-[60px] flex items-center'>
           <div className='mr-[32px] w-[320px] text-right'>
             <div className='text-4xl'>Artists</div>
             <div className='text-xl text-gray-600'>
@@ -228,29 +276,220 @@ const Home: NextPage<Props> = ({ fallback }) => {
       {/* ------------------ FEATURED ART ------------------ */}
 
       <div className='col-span-full mt-[90px] mb-[90px] w-full border-t border-gray-700'></div>
-      <div className='col-span-full w-full ml-[30px]'>
-        <div className='text-4xl'>Featured Art</div>
-        <div className='float-right'>
-          <Link href='https://niftyapes.readme.io/docs' target='_blank'>
-            <div
-              className='flex flex-row items-center rounded-full bg-black py-5 px-8 text-center uppercase text-white'>
-              <span className='text-lg'>explore</span>
-              <div
-                className="ml-[15px] h-[24px] w-[24px] rounded-full border border-white bg-[url('/icons/arrow-up-right.svg')] bg-center bg-no-repeat"></div>
+
+      <div className='flex col-span-full w-full items-center justify-center mb-[90px]'>
+        <div className='col-span-full max-w-[1095px]'>
+
+          <div className='flex px-[30px] mb-[48px]'>
+            <div>
+              <div className='text-4xl'>Featured Art</div>
+              <div className='text-gray-600 flex items-center mt-[24px]'>
+                <div>@xcopy</div>
+                <div className='ml-[15px] mr-[15px] h-[6px] w-[6px] rounded-full bg-gray-700'></div>
+                <div>@tylerxhobbs</div>
+                <div className='ml-[15px] mr-[15px] h-[6px] w-[6px] rounded-full bg-gray-700'></div>
+                <div>@dmitricherniak</div>
+                <div className='ml-[15px] mr-[15px] h-[6px] w-[6px] rounded-full bg-gray-700'></div>
+                <div>@gremplin</div>
+                <div className='ml-[15px] mr-[15px] h-[6px] w-[6px] rounded-full bg-gray-700'></div>
+                <div>@xboddy</div>
+              </div>
             </div>
-          </Link>
-        </div>
+            <div className='ml-auto'>
+              <Link href='https://niftyapes.readme.io/docs' target='_blank'>
+                <div
+                  className='flex items-center uppercase text-white'>
+                  <span className='text-lg'>explore</span>
+                  <div
+                    className="ml-[15px] h-[24px] w-[24px] rounded-full border border-white bg-[url('/icons/arrow-up-right.svg')] bg-center bg-no-repeat"></div>
+                </div>
+              </Link>
+            </div>
+          </div>
 
-        <div className='text-gray-600 flex items-center mt-[24px]'>
-          <div>@xcopy</div>
-          <div className='ml-[15px] mr-[15px] h-[6px] w-[6px] rounded-full bg-gray-700'></div>
-          <div>@tylerxhobbs</div>
-          <div className='ml-[15px] mr-[15px] h-[6px] w-[6px] rounded-full bg-gray-700'></div>
-          <div>@dmitricherniak</div>
-          <div className='ml-[15px] mr-[15px] h-[6px] w-[6px] rounded-full bg-gray-700'></div>
-          <div>@gremplin</div>
-        </div>
+          <div>
+            <div className='grid grid-cols-4 w-full space-x-[24px] items-end'>
+              <div className="relative group">
+                <div className='absolute z-50 bg-white py-[14px] px-[18px] right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+                  <div className='mb-3'>
+                    <span className='text-sm text-gray-500 mr-2'>xcopy</span>
+                    <span className='text-base text-black'>artwork</span>
+                  </div>
+                  <div className='flex'>
+                    <div>
+                      <div className='text-black text-xs'>243 ETH</div>
+                      <div className='text-[10px] uppercase text-gray-500'>price</div>
+                    </div>
+                    <div className='border-t border-r border-gray-200 ml-5 mr-5'></div>
+                    <div>
+                      <div className='text-black text-xs'>22 ETH</div>
+                      <div className='text-[10px] uppercase text-gray-500'>
+                        buy with financing
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div style={{ background: 'linear-gradient(90deg, transparent, rgba(0, 0, 0, 0.75))' }} className='w-full h-full absolute bottom-0 right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+                </div>
+                <img
+                  src='https://pixura.imgix.net/https%3A%2F%2Fstorage.googleapis.com%2Fsr_prod_artworks_bucket%2F0x8a1521398214a61bef23e0a2f2909a6a152f9341%252F182%252F7da39fb1-a2ec-40b6-8c78-2dda40e3bcc7%252Furi%252Fimage-2023-08-02-10-56-vrycn?ixlib=js-3.8.0&w=3000&fit=clip&q=100&auto=format&s=eeb1097733881fd47ffb75bd9d966453' />
+              </div>
+              <div className="relative group">
+                <div className='absolute z-50 bg-white py-[14px] px-[18px] right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+                  <div className='mb-3'>
+                    <span className='text-sm text-gray-500 mr-2'>xcopy</span>
+                    <span className='text-base text-black'>artwork</span>
+                  </div>
+                  <div className='flex'>
+                    <div>
+                      <div className='text-black text-xs'>243 ETH</div>
+                      <div className='text-[10px] uppercase text-gray-500'>price</div>
+                    </div>
+                    <div className='border-t border-r border-gray-200 ml-5 mr-5'></div>
+                    <div>
+                      <div className='text-black text-xs'>22 ETH</div>
+                      <div className='text-[10px] uppercase text-gray-500'>
+                        buy with financing
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div style={{ background: 'linear-gradient(90deg, transparent, rgba(0, 0, 0, 0.75))' }} className='w-full h-full absolute bottom-0 right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+                </div>
+                <img
+                  src='https://pixura.imgix.net/https%3A%2F%2Fstorage.googleapis.com%2Fsr_prod_artworks_bucket%2F0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0%252F45620%252F9f5cf41c-6869-40c9-b286-acf042bac3d5%252Furi%252Fimage-2023-07-27-00-44-xfyag?ixlib=js-3.8.0&w=1080&fit=clip&q=100&auto=format&s=bbe92020ff2c95e50409189908f3c10a' />
+              </div>
+              <div className="relative group">
+                <div className='absolute z-50 bg-white py-[14px] px-[18px] right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+                  <div className='mb-3'>
+                    <span className='text-sm text-gray-500 mr-2'>xcopy</span>
+                    <span className='text-base text-black'>artwork</span>
+                  </div>
+                  <div className='flex'>
+                    <div>
+                      <div className='text-black text-xs'>243 ETH</div>
+                      <div className='text-[10px] uppercase text-gray-500'>price</div>
+                    </div>
+                    <div className='border-t border-r border-gray-200 ml-5 mr-5'></div>
+                    <div>
+                      <div className='text-black text-xs'>22 ETH</div>
+                      <div className='text-[10px] uppercase text-gray-500'>
+                        buy with financing
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div style={{ background: 'linear-gradient(90deg, transparent, rgba(0, 0, 0, 0.75))' }} className='w-full h-full absolute bottom-0 right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+                </div>
+                <img
+                  src='https://pixura.imgix.net/https%3A%2F%2Fstorage.googleapis.com%2Fsr_prod_artworks_bucket%2F0x72017a15cc866c82a7a3b4f147bca625a2e0a4ce%252F114%252Fdb1a8717-b4d9-4185-8ecf-0f08b5665c56%252Furi%252Fimage-2023-07-31-17-42-nzsi3?ixlib=js-3.8.0&h=1080&fit=clip&q=100&auto=format&s=7f84f0db8e1b40edb8228a011dbd2e30' />
+              </div>
+              <div className="relative group">
+                <div className='absolute z-50 bg-white py-[14px] px-[18px] right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+                  <div className='mb-3'>
+                    <span className='text-sm text-gray-500 mr-2'>xcopy</span>
+                    <span className='text-base text-black'>artwork</span>
+                  </div>
+                  <div className='flex'>
+                    <div>
+                      <div className='text-black text-xs'>243 ETH</div>
+                      <div className='text-[10px] uppercase text-gray-500'>price</div>
+                    </div>
+                    <div className='border-t border-r border-gray-200 ml-5 mr-5'></div>
+                    <div>
+                      <div className='text-black text-xs'>22 ETH</div>
+                      <div className='text-[10px] uppercase text-gray-500'>
+                        buy with financing
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div style={{ background: 'linear-gradient(90deg, transparent, rgba(0, 0, 0, 0.75))' }} className='w-full h-full absolute bottom-0 right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+                </div>
+                <img
+                  src='https://pixura.imgix.net/https%3A%2F%2Fstorage.googleapis.com%2Fsr_prod_artworks_bucket%2F0x8a1521398214a61bef23e0a2f2909a6a152f9341%252F114%252F9287e8b8-8d73-460d-b474-b32d9d6387ca%252Furi%252Fimage-2023-07-22-10-29-yyi4n?ixlib=js-3.8.0&w=1080&fit=clip&q=100&auto=format&s=938d5497f0fbf121786fc992b92dd562' />
+              </div>
+            </div>
+            <div className='grid grid-cols-3 w-full mt-[24px] space-x-[24px] items-start'>
+              <div className="relative group">
+                <div className='absolute z-50 bg-white py-[14px] px-[18px] right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+                  <div className='mb-3'>
+                    <span className='text-sm text-gray-500 mr-2'>xcopy</span>
+                    <span className='text-base text-black'>artwork</span>
+                  </div>
+                  <div className='flex'>
+                    <div>
+                      <div className='text-black text-xs'>243 ETH</div>
+                      <div className='text-[10px] uppercase text-gray-500'>price</div>
+                    </div>
+                    <div className='border-t border-r border-gray-200 ml-5 mr-5'></div>
+                    <div>
+                      <div className='text-black text-xs'>22 ETH</div>
+                      <div className='text-[10px] uppercase text-gray-500'>
+                        buy with financing
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div style={{ background: 'linear-gradient(90deg, transparent, rgba(0, 0, 0, 0.75))' }} className='w-full h-full absolute bottom-0 right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+                </div>
+                <img
+                src='https://pixura.imgix.net/https%3A%2F%2Fstorage.googleapis.com%2Fsr_prod_artworks_bucket%2F0x595f7f5170c16fa9812f5d067e2818cfa9042bc1%252F1%252F4409210f-a365-4a56-b2ba-46a3cbe3752e%252Furi%252Fimage-2023-08-01-11-43-l3kfr?ixlib=js-3.8.0&w=1080&fit=clip&q=100&auto=format&s=ee970a0483ce5abd4d2538609bfc499a' />
+              </div>
+              <div className="relative group">
+                <div className='absolute z-50 bg-white py-[14px] px-[18px] right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+                  <div className='mb-3'>
+                    <span className='text-sm text-gray-500 mr-2'>xcopy</span>
+                    <span className='text-base text-black'>artwork</span>
+                  </div>
+                  <div className='flex'>
+                    <div>
+                      <div className='text-black text-xs'>243 ETH</div>
+                      <div className='text-[10px] uppercase text-gray-500'>price</div>
+                    </div>
+                    <div className='border-t border-r border-gray-200 ml-5 mr-5'></div>
+                    <div>
+                      <div className='text-black text-xs'>22 ETH</div>
+                      <div className='text-[10px] uppercase text-gray-500'>
+                        buy with financing
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div style={{ background: 'linear-gradient(90deg, transparent, rgba(0, 0, 0, 0.75))' }} className='w-full h-full absolute bottom-0 right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+                </div>
+                <img
+                src='https://pixura.imgix.net/https%3A%2F%2Fstorage.googleapis.com%2Fsr_prod_artworks_bucket%2F0x96bd48df606c8a241c263c72d57244272d079b38%252F30%252F8364950b-68bf-45c6-bdb8-e6453fa18136%252Furi%252Fimage-2023-07-28-14-13-q2iv3?ixlib=js-3.8.0&w=1080&fit=clip&q=100&auto=format&s=bd8ac9fad1a6153fa2dbdf16609f0f76' />
+              </div>
+              <div className="relative group">
+                <div className='absolute z-50 bg-white py-[14px] px-[18px] right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+                  <div className='mb-3'>
+                    <span className='text-sm text-gray-500 mr-2'>xcopy</span>
+                    <span className='text-base text-black'>artwork</span>
+                  </div>
+                  <div className='flex'>
+                    <div>
+                      <div className='text-black text-xs'>243 ETH</div>
+                      <div className='text-[10px] uppercase text-gray-500'>price</div>
+                    </div>
+                    <div className='border-t border-r border-gray-200 ml-5 mr-5'></div>
+                    <div>
+                      <div className='text-black text-xs'>22 ETH</div>
+                      <div className='text-[10px] uppercase text-gray-500'>
+                        buy with financing
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div style={{ background: 'linear-gradient(90deg, transparent, rgba(0, 0, 0, 0.75))' }} className='w-full h-full absolute bottom-0 right-0 opacity-0 group-hover:opacity-100 transition-opacity duration-100'>
+                </div>
+                <img
+                src='https://ipfs.pixura.io/ipfs/QmWF8CamCB9Ar5i8F5J7qQGa8PVF281PBo1FUgNHHHtgBY/THETHINKER.JPG' />
+              </div>
+            </div>
 
+          </div>
+        </div>
       </div>
 
 
