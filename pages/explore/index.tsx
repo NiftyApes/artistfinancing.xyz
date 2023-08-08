62b0dc6d56a9ae083b09a320af49ba650be7eca0
andreitr <andrei.taraschuk@gmail.com>
2023-08-08 16:21:40 -0600

    Page meta and misc layout updates

--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -1,27 +1,54 @@
 import Layout from 'components/Layout'
 import { NextPage } from 'next'
 import Head from 'next/head'
-
-import { TokenDetails } from 'types/reservoir'
-import FeaturedFinancingOffers from '../../components/FeaturedFinancingOffers'
 import Footer from '../../components/Footer'
+import FeaturedFinancingOffers from '../../components/FeaturedFinancingOffers'
 
-type Props = {
-  collectionId: string
-  tokenDetails?: TokenDetails
+
+const META_TITLE = process.env.NEXT_PUBLIC_META_TITLE
+const META_DESCRIPTION = process.env.NEXT_PUBLIC_META_DESCRIPTION
+const META_IMAGE = process.env.NEXT_PUBLIC_META_OG_IMAGE
+
+const metadata = {
+  title: (title: string) => <title>{title}</title>,
+  description: (description: string) => (
+    <meta name='description' content={description} />
+  ),
+  image: (image?: string) => {
+    if (image) {
+      return (
+        <>
+          <meta name='twitter:image' content={image} />
+          <meta name='og:image' content={image} />
+        </>
+      )
+    }
+    return null
+  }
 }
-const Explore: NextPage<Props> = () => {
+
+
+const Explore: NextPage = () => {
+
+  const title = META_TITLE && metadata.title(META_TITLE)
+  const description = META_DESCRIPTION && metadata.description(META_DESCRIPTION)
+  const image = metadata.image(META_IMAGE)
 
   return (
     <Layout navbar={{}}>
       <Head>
-        {'Explore'}
-        {'Explore'}
+        {title}
+        {description}
+        {image}
       </Head>
 
-      <div className='col-span-full mt-20 mb-20'>
+      <div className='col-span-full mt-20 mb-20 flex flex-col items-center justify-center'>
+        <div className='text-5xl'>Buy Now, Use Now, Pay Later</div>
+        <div className='mt-5 text-xl font-thin text-gray-600'>A new primitive enabling easy financing for every NFT.</div>
+      </div>
 
-        <div className='font-extrabold text-3xl mb-[48px] ml-20'>Buy Now</div>
+      <div className='col-span-full mb-20'>
+
         <div className='flex items-center justify-center px-4'>
           <FeaturedFinancingOffers />
         </div>
