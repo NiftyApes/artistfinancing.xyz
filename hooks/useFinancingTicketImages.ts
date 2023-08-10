2bd3cc60b287fa00b0c55de32cf67895a0c23503
Chris Lee <chris.r.lee43@gmail.com>
2023-08-10 16:05:35 -0700

    Update seller and buyer ticket images

--- hooks/useFinancingTicketImages.ts
+++ hooks/useFinancingTicketImages.ts
@@ -4,27 +4,31 @@ import isEqualAddress from 'lib/isEqualAddress'
 import { useSellerFinancingContract } from '@niftyapes/sdk'
 
 // Adds token images to each NiftyApes_SellerFinancingReceipt NFT
-export function useNiftyApesImages(
-  tokens:
-    | ReturnType<typeof useTokens>['data']
-    | ReturnType<typeof useUserTokens>['data']
-) {
+export const useFinancingTicketImages = () => {
   const { address } = useSellerFinancingContract()
   const mounted = useMounted() // Check mounted to access 'window'
 
-  tokens.forEach((token) => {
-    if (!isEqualAddress(token?.token?.contract, address)) {
-      return
-    }
+  return {
+    updateTokenImages(
+      tokens:
+        | ReturnType<typeof useTokens>['data']
+        | ReturnType<typeof useUserTokens>['data']
+    ) {
+      tokens.forEach((token) => {
+        if (!isEqualAddress(token?.token?.contract, address)) {
+          return
+        }
 
-    const nftId = token?.token?.tokenId
-    if (!nftId || !token.token || !mounted) {
-      return
-    }
+        const nftId = token?.token?.tokenId
+        if (!nftId || !token.token || !mounted) {
+          return
+        }
 
-    token.token.image =
-      Number(nftId) % 2 === 0
-        ? `${window.location.origin}/niftyapes/buyer-ticket.png`
-        : `${window.location.origin}/niftyapes/seller-ticket.png`
-  })
+        token.token.image =
+          Number(nftId) % 2 === 0
+            ? `${window.location.origin}/niftyapes/ticket-buyer.png`
+            : `${window.location.origin}/niftyapes/ticket-seller.png`
+      })
+    },
+  }
 }
