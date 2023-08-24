36396713f8802f001ad85a70800a3a02f0a086bd
andreitr <andrei.taraschuk@gmail.com>
2023-08-24 13:17:31 -0600

    explore page title

--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -40,9 +40,9 @@ const Explore: NextPage = () => {
       </Head>
 
       <div className="col-span-full mt-20 mb-20 flex flex-col items-center justify-center">
-        <div className="text-5xl">Buy Now, Use Now, Pay Later</div>
+        <div className="text-5xl">Collect with Financing</div>
         <div className="mt-5 text-xl font-thin text-gray-600">
-          A new primitive enabling easy financing for every NFT.
+          Flexible terms from premium artists
         </div>
       </div>
 
