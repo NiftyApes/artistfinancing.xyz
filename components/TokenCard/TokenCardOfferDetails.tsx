1dc822f068f78c85f25db6e5760f5604129470e2
Chris Lee <chris.r.lee43@gmail.com>
2023-09-07 12:42:40 -0700

    Remove duplicate components

--- components/TokenCard/TokenCardOfferDetails.tsx
+++ components/TokenCard/TokenCardOfferDetails.tsx
@@ -1,13 +1,22 @@
 import { Offer } from '@niftyapes/sdk'
 import { processOffer } from 'lib/processOffer'
 
-export default function TokeCardOfferDetails({ offer }: { offer: Offer }) {
+export default function TokenCardOfferDetails({ offer }: { offer: Offer }) {
   const terms = processOffer(offer.offer)
+
   return (
-    <div className="mt-2">
-      <div className="text-[13px] text-gray-400">List Price</div>
-      <div className="text-[13px] font-semibold text-gray-400">
-        {terms.listPrice}Ξ
+    <div className="mt-2 flex space-x-4">
+      <div className="w-1/2">
+        <div className="text-[13px] text-gray-400">Down Payment</div>
+        <div className="text-[13px] font-semibold text-gray-400">
+          {terms.listPrice}Ξ
+        </div>
+      </div>
+      <div className="w-1/2">
+        <div className="text-[13px] text-gray-400">Monthly</div>
+        <div className="text-[13px] font-semibold text-gray-400">
+          {terms.listPrice}Ξ
+        </div>
       </div>
     </div>
   )
