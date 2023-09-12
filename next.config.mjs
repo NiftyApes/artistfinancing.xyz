bfa41f6c175bf1f2e05de9f727358fb34925b545
Chris Lee <chris.r.lee43@gmail.com>
2023-09-12 13:31:20 -0700

    Remove the need for yalc

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,22 @@
+import path from 'path'
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@reservoir0x/reservoir-kit-ui', '@niftyapes/sdk'],
+  webpack: (config) => {
+    // Add alias for `wagmi`, `@wagmi/core`, and any other peer dependencies
+    config.resolve.alias['wagmi'] = path.resolve('./node_modules/wagmi')
+    config.resolve.alias['@wagmi/core'] = path.resolve(
+      './node_modules/@wagmi/core'
+    )
+    // ... Add other aliases as needed
+
+    // Return the modified config
+    return config
+  },
 }
 
 export default nextConfig
